93ce457cb576317129605ed201d019fcb15ca768
Glenn Renfro <grenfro@vmware.com>
2022-03-10 16:46:30 -0500

    Updated with deprecation
    Updated based on code review

--- spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/DeployerSocketUtils.java
+++ spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/DeployerSocketUtils.java
@@ -45,7 +45,8 @@ import org.springframework.util.Assert;
  * @author Gary Russell
  * @author Glenn Renfro
  */
-public abstract class DeployerSocketUtils {
+@Deprecated
+public class DeployerSocketUtils {
 
 	/**
 	 * The default maximum value for port ranges used when finding an available socket
@@ -58,120 +59,136 @@ public abstract class DeployerSocketUtils {
 	/**
 	 * Find an available TCP port randomly selected from the range [{@code minPort},
 	 * {@value #PORT_RANGE_MAX}].
-	 *
 	 * @param minPort the minimum port number
 	 * @return an available TCP port number
 	 * @throws IllegalStateException if no available port could be found
 	 */
 	public static int findAvailableTcpPort(int minPort) {
-		return findAvailablePort(minPort, PORT_RANGE_MAX);
+		return findAvailableTcpPort(minPort, PORT_RANGE_MAX);
+	}
+
+	/**
+	 * Find an available TCP port randomly selected from the range [{@code minPort},
+	 * {@code maxPort}].
+	 * @param minPort the minimum port number
+	 * @param maxPort the maximum port number
+	 * @return an available TCP port number
+	 * @throws IllegalStateException if no available port could be found
+	 */
+	public static int findAvailableTcpPort(int minPort, int maxPort) {
+		return SocketType.TCP.findAvailablePort(minPort, maxPort);
 	}
 
 	/**
 	 * Find the requested number of available TCP ports, each randomly selected from the
 	 * range [{@code minPort}, {@code maxPort}].
-	 *
 	 * @param numRequested the number of available ports to find
-	 * @param minPort      the minimum port number
-	 * @param maxPort      the maximum port number
+	 * @param minPort the minimum port number
+	 * @param maxPort the maximum port number
 	 * @return a sorted set of available TCP port numbers
 	 * @throws IllegalStateException if the requested number of available ports could not
-	 *                               be found
+	 * be found
 	 */
 	public static SortedSet<Integer> findAvailableTcpPorts(int numRequested, int minPort, int maxPort) {
-		return findAvailablePorts(numRequested, minPort, maxPort);
+		return SocketType.TCP.findAvailablePorts(numRequested, minPort, maxPort);
 	}
 
-	private static boolean isPortAvailable(int port) {
-		try {
-			ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(port, 1,
-					InetAddress.getByName("localhost"));
-			serverSocket.close();
-			return true;
-		}
-		catch (Exception ex) {
-			return false;
-		}
-	}
-
-	/**
-	 * Find a pseudo-random port number within the range [{@code minPort},
-	 * {@code maxPort}].
-	 *
-	 * @param minPort the minimum port number
-	 * @param maxPort the maximum port number
-	 * @return a random port number within the specified range
-	 */
-	private static int findRandomPort(int minPort, int maxPort) {
-		int portRange = maxPort - minPort;
-		return minPort + random.nextInt(portRange + 1);
-	}
-
-	/**
-	 * Find an available port for this {@code SocketType}, randomly selected from the
-	 * range [{@code minPort}, {@code maxPort}].
-	 *
-	 * @param minPort the minimum port number
-	 * @param maxPort the maximum port number
-	 * @return an available port number for this socket type
-	 * @throws IllegalStateException if no available port could be found
-	 */
-	private static int findAvailablePort(int minPort, int maxPort) {
-		Assert.isTrue(minPort > 0, "'minPort' must be greater than 0");
-		Assert.isTrue(maxPort >= minPort, "'maxPort' must be greater than or equal to 'minPort'");
-		Assert.isTrue(maxPort <= PORT_RANGE_MAX, "'maxPort' must be less than or equal to " + PORT_RANGE_MAX);
-
-		int portRange = maxPort - minPort;
-		int candidatePort;
-		int searchCounter = 0;
-		do {
-			if (searchCounter > portRange) {
-				throw new IllegalStateException(
-						String.format("Could not find an available TCP port in the range [%d, %d] after %d attempts",
-								minPort, maxPort, searchCounter));
+	private enum SocketType {
+		TCP {
+			@Override
+			protected boolean isPortAvailable(int port) {
+				try {
+					ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(port, 1,
+							InetAddress.getByName("localhost"));
+					serverSocket.close();
+					return true;
+				}
+				catch (Exception ex) {
+					return false;
+				}
 			}
-			candidatePort = findRandomPort(minPort, maxPort);
-			searchCounter++;
-		}
-		while (!isPortAvailable(candidatePort));
+		};
 
-		return candidatePort;
+		/**
+		 * Determine if the specified port for this {@code SocketType} is currently
+		 * available on {@code localhost}.
+		 */
+		protected abstract boolean isPortAvailable(int port);
+
+		/**
+		 * Find a pseudo-random port number within the range [{@code minPort},
+		 * {@code maxPort}].
+		 * @param minPort the minimum port number
+		 * @param maxPort the maximum port number
+		 * @return a random port number within the specified range
+		 */
+		private int findRandomPort(int minPort, int maxPort) {
+			int portRange = maxPort - minPort;
+			return minPort + random.nextInt(portRange + 1);
+		}
+
+		/**
+		 * Find an available port for this {@code SocketType}, randomly selected from the
+		 * range [{@code minPort}, {@code maxPort}].
+		 * @param minPort the minimum port number
+		 * @param maxPort the maximum port number
+		 * @return an available port number for this socket type
+		 * @throws IllegalStateException if no available port could be found
+		 */
+		int findAvailablePort(int minPort, int maxPort) {
+			Assert.isTrue(minPort > 0, "'minPort' must be greater than 0");
+			Assert.isTrue(maxPort >= minPort, "'maxPort' must be greater than or equal to 'minPort'");
+			Assert.isTrue(maxPort <= PORT_RANGE_MAX, "'maxPort' must be less than or equal to " + PORT_RANGE_MAX);
+
+			int portRange = maxPort - minPort;
+			int candidatePort;
+			int searchCounter = 0;
+			do {
+				if (searchCounter > portRange) {
+					throw new IllegalStateException(
+							String.format("Could not find an available %s port in the range [%d, %d] after %d attempts",
+									name(), minPort, maxPort, searchCounter));
+				}
+				candidatePort = findRandomPort(minPort, maxPort);
+				searchCounter++;
+			}
+			while (!isPortAvailable(candidatePort));
+
+			return candidatePort;
+		}
+
+		/**
+		 * Find the requested number of available ports for this {@code SocketType}, each
+		 * randomly selected from the range [{@code minPort}, {@code maxPort}].
+		 * @param numRequested the number of available ports to find
+		 * @param minPort the minimum port number
+		 * @param maxPort the maximum port number
+		 * @return a sorted set of available port numbers for this socket type
+		 * @throws IllegalStateException if the requested number of available ports could
+		 * not be found
+		 */
+		SortedSet<Integer> findAvailablePorts(int numRequested, int minPort, int maxPort) {
+			Assert.isTrue(minPort > 0, "'minPort' must be greater than 0");
+			Assert.isTrue(maxPort > minPort, "'maxPort' must be greater than 'minPort'");
+			Assert.isTrue(maxPort <= PORT_RANGE_MAX, "'maxPort' must be less than or equal to " + PORT_RANGE_MAX);
+			Assert.isTrue(numRequested > 0, "'numRequested' must be greater than 0");
+			Assert.isTrue((maxPort - minPort) >= numRequested,
+					"'numRequested' must not be greater than 'maxPort' - 'minPort'");
+
+			SortedSet<Integer> availablePorts = new TreeSet<>();
+			int attemptCount = 0;
+			while ((++attemptCount <= numRequested + 100) && availablePorts.size() < numRequested) {
+				availablePorts.add(findAvailablePort(minPort, maxPort));
+			}
+
+			if (availablePorts.size() != numRequested) {
+				throw new IllegalStateException(
+						String.format("Could not find %d available %s ports in the range [%d, %d]", numRequested,
+								name(), minPort, maxPort));
+			}
+
+			return availablePorts;
+		}
 	}
-
-	/**
-	 * Find the requested number of available ports for this {@code SocketType}, each
-	 * randomly selected from the range [{@code minPort}, {@code maxPort}].
-	 *
-	 * @param numRequested the number of available ports to find
-	 * @param minPort      the minimum port number
-	 * @param maxPort      the maximum port number
-	 * @return a sorted set of available port numbers for this socket type
-	 * @throws IllegalStateException if the requested number of available ports could
-	 *                               not be found
-	 */
-	private static SortedSet<Integer> findAvailablePorts(int numRequested, int minPort, int maxPort) {
-		Assert.isTrue(minPort > 0, "'minPort' must be greater than 0");
-		Assert.isTrue(maxPort > minPort, "'maxPort' must be greater than 'minPort'");
-		Assert.isTrue(maxPort <= PORT_RANGE_MAX, "'maxPort' must be less than or equal to " + PORT_RANGE_MAX);
-		Assert.isTrue(numRequested > 0, "'numRequested' must be greater than 0");
-		Assert.isTrue((maxPort - minPort) >= numRequested,
-				"'numRequested' must not be greater than 'maxPort' - 'minPort'");
-
-		SortedSet<Integer> availablePorts = new TreeSet<>();
-		int attemptCount = 0;
-		while ((++attemptCount <= numRequested + 100) && availablePorts.size() < numRequested) {
-			availablePorts.add(findAvailablePort(minPort, maxPort));
-		}
-
-		if (availablePorts.size() != numRequested) {
-			throw new IllegalStateException(
-					String.format("Could not find %d available TCP ports in the range [%d, %d]",
-							numRequested, minPort, maxPort));
-		}
-
-		return availablePorts;
-	}
-
-
 }
 
